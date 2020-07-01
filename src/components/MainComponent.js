b64d8f022a083ea2f8f36c66cb54725e1f22ef6d
Weiwei <ww.zhang@mail.utoronto.ca>
2020-07-01 14:57:45 -0400

    react animation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { actions } from 'react-redux-form';
+import {TransitionGroup,CSSTransition} from 'react-transition-group';
 
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
@@ -46,14 +47,18 @@ class Main extends Component{
         return (
         <div>
             <Header />
-            <Switch>
-                <Route path='/home' component={HomePage} />
-                <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders} />} />
-                <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>} />
-                <Route path='/menu/:dishId' component={DishWithId} />
-                <Route exact path='/contactus' component={()=> <Contact resetFeedbackForm = {this.props.resetFeedbackForm} />} />
-                <Redirect to='/home' />
-            </Switch>
+            <TransitionGroup>
+                <CSSTransition key={this.props.location.key} classNames='page' timeout={300}>
+                    <Switch>
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders} />} />
+                        <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>} />
+                        <Route path='/menu/:dishId' component={DishWithId} />
+                        <Route exact path='/contactus' component={()=> <Contact resetFeedbackForm = {this.props.resetFeedbackForm} />} />
+                        <Redirect to='/home' />
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
             <Footer />
         </div>
         );
